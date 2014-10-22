2de4cf564c2b09d2a4dd2fe1c4bdde595efb2396
ju <ju@JULIAN>
2014-10-22 18:35:59 -0500

    hola feo

--- calculadora/src/main/Main.java
+++ calculadora/src/main/Main.java
@@ -1,5 +1,21 @@
 package main;
-
+import suma.Suma;
 public class Main {
+    private static int number1;
+    private static int number2;
+    public Main() {
+		number1=1;
+		number2=2;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	   int resultado=0;
+	   Suma sum=new Suma(number1,number2);
+       resultado=sum.getResultado();
+      
+	}
 
 }
---
+++ calculadora/src/suma/Suma.java
@@ -0,0 +1,30 @@
+package suma;
+
+public class Suma {
+    private int sumando1;
+    private int sumando2;
+    private int resultado;
+	public int getSumando1() {
+		return sumando1;
+	}
+	public void setSumando1(int sumando1) {
+		this.sumando1 = sumando1;
+	}
+	public int getSumando2() {
+		return sumando2;
+	}
+	public void setSumando2(int sumando2) {
+		this.sumando2 = sumando2;
+	}
+    public Suma(int s1,int s2){
+    	this.setResultado(s1+s2);
+    }
+	public int getResultado() {
+		return resultado;
+	}
+	public void setResultado(int resultado) {
+		this.resultado = resultado;
+	}
+    
+	
+}
